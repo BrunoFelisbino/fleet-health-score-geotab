a9ae153dd07952957ce62032e8c75c03502e1d09
Bruno Felisbino <52917772+BrunoFelisbino@users.noreply.github.com>
2026-06-15 17:10:18 -0300

    Use no logo report generator

--- reports/boot-v41.js
+++ reports/boot-v41.js
@@ -1,4 +1,3 @@
-import './logo-data-fetch-v46.js?v=0.4.6';
-import './canvas-table-text-fix-v45.js?v=0.4.6';
-import './premium-removal.js?v=0.4.6';
-import './pdf-canvas-removal.js?v=0.4.6';
+import './canvas-table-text-fix-v45.js?v=0.4.7';
+import './premium-removal.js?v=0.4.7';
+import './no-logo-stable-v47.js?v=0.4.7';
